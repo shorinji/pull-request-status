6e7629ea33c026c834b73334181348123d24173f
Fredrik Fall <fredrik.fall@etraveligroup.com>
2019-06-26 08:57:49 +0200

    Removed curl dependency to use native https module

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const child_process = require("child_process");
-const config = require('./config');
-
+const https = require("https");
+const config = require("./config");
 
 const abbreviateReviewerName = reviewer => {
   const parts = reviewer.user.displayName.split(" ");
@@ -8,18 +7,15 @@ const abbreviateReviewerName = reviewer => {
   return parts[0][0] + parts[parts.length - 1][0];
 };
 
-const parseResponse = (error, stdout, stderr) => {
-  if (error) {
-    console.error(error);
-    return null;
-  }
+const parseResponse = (httpResponse, callback) => {
+  let body = "";
+  httpResponse.on("data", chunk => {
+    body += chunk;
+  });
 
-  try {
-    return JSON.parse(stdout);
-  } catch (e) {
-    console.error(e);
-  }
-  return null;
+  httpResponse.on("end", () => {
+    callback(JSON.parse(body));
+  });
 };
 
 const checkConfig = config => {
@@ -32,82 +28,93 @@ const checkConfig = config => {
   return configOk;
 };
 
-
 if (!checkConfig(config)) {
   console.error("Please add your settings to config.js");
   process.exit(-1);
 }
 
-if (!child_process.execSync("which curl")) {
-  console.error("This cool script requires curl to be installed in your path. Please install it and try again");
-  process.exit(-1);
-}
+// -------------------------------------------------------
 
+const requestOptions = {
+  hostname: config.baseUrl,
+  path: `/rest/api/1.0/projects/${config.project}/repos/${
+    config.repo
+  }/pull-requests`,
+  headers: {
+    Authorization: `Bearer ${config.token}`
+  }
+};
 
-const authorizationHeader = `Authorization: Bearer ${config.token}`;
-
-const prUrl =
-  `${config.baseUrl}/rest/api/1.0/projects/${config.project}/repos/${config.repo}/pull-requests`;
-const cmd = `curl -H "Content-Type: application/json" -H "${authorizationHeader}" ${prUrl}`;
-
-
-child_process.exec(cmd, (error, stdout, stderr) => {
-  const response = parseResponse(error, stdout, stderr);
-  if (!response) {
-    console.error("bailing out...");
+https.get(requestOptions, response => {
+  if (response.statusCode !== 200) {
+    console.error(response.statusMessage);
     return;
   }
-  const keys = ["id", "state", "open", "closed", "title"];
 
-  const whiteAnsiColor = 37;
-  const greenAnsiColor = 42;
-  const yellowAnsiColor = 43;
+  parseResponse(response, jsonResponse => {
+    if (!jsonResponse) {
+      console.error("bailing out...");
+      return;
+    }
+    const keys = ["id", "state", "open", "closed", "title"];
 
-  response.values.forEach((pr, index) => {
-    const cmd = `curl -H "Content-Type: application/json" -H "${authorizationHeader}" ${prUrl}/${
-      pr.id
-    }`;
-    child_process.exec(cmd, (error, stdout, stderr) => {
-      const response = parseResponse(error, stdout, stderr);
-      if (!response) {
-        console.error(error);
-        return;
-      }
+    const whiteAnsiColor = 37;
+    const greenAnsiColor = 42;
+    const yellowAnsiColor = 43;
 
-      const approvers = response.reviewers.filter(r => r.approved);
-      const approverNames = approvers
-        .map(abbreviateReviewerName)
-        .map(name => "\x1b[" + greenAnsiColor + ";30m " + name + " \x1b[0m")
-        .join(" ");
+    jsonResponse.values.forEach((pr, index) => {
+      const requestOptions = {
+        hostname: config.baseUrl,
+        path: `/rest/api/1.0/projects/${config.project}/repos/${
+          config.repo
+        }/pull-requests/${pr.id}`,
+        headers: {
+          Authorization: `Bearer ${config.token}`
+        }
+      };
 
-      const unapprovers = response.reviewers.filter(r => !r.approved);
-      const unapproverNames = unapprovers
-        .map(abbreviateReviewerName)
-        .map(name => "\x1b[" + yellowAnsiColor + ";30m " + name + " \x1b[0m")
-        .join(" ");
+      https.get(requestOptions, response => {
+        if (response.statusCode !== 200) {
+          console.error(response.statusMessage);
+          return;
+        }
 
-      const author = response.author.user.displayName;
+        parseResponse(response, jsonResponse => {
+          const approvers = jsonResponse.reviewers.filter(r => r.approved);
+          const approverNames = approvers
+            .map(abbreviateReviewerName)
+            .map(name => "\x1b[" + greenAnsiColor + ";30m " + name + " \x1b[0m")
+            .join(" ");
 
-      console.log("\n");
-      
-      console.log(
-        `[${response.id}] ${response.title}  [${
-          response.author.user.name === config.username
-            ? "\x1b[" + whiteAnsiColor + ";1m" + author + "\x1b[0m"
-            : author
-        }]`
-      );
-      console.log(
-        "UNAPPROVED  %s  %s",
-        (unapprovers.length < 10 ? " (" : "(") + unapprovers.length + ")",
-        unapproverNames
-      );
-      console.log(
-        "APPROVED    %s  %s",
-        (approvers.length < 10 ? " (" : "(") + approvers.length + ")",
-        approverNames
-      );
+          const unapprovers = jsonResponse.reviewers.filter(r => !r.approved);
+          const unapproverNames = unapprovers
+            .map(abbreviateReviewerName)
+            .map(
+              name => "\x1b[" + yellowAnsiColor + ";30m " + name + " \x1b[0m"
+            )
+            .join(" ");
 
+          const author = jsonResponse.author.user.displayName;
+
+          console.log(
+            `\n[${jsonResponse.id}] ${jsonResponse.title}  [${
+              jsonResponse.author.user.name === config.username
+                ? "\x1b[" + whiteAnsiColor + ";1m" + author + "\x1b[0m"
+                : author
+            }]`
+          );
+          console.log(
+            "UNAPPROVED  %s  %s",
+            (unapprovers.length < 10 ? " (" : "(") + unapprovers.length + ")",
+            unapproverNames
+          );
+          console.log(
+            "APPROVED    %s  %s",
+            (approvers.length < 10 ? " (" : "(") + approvers.length + ")",
+            approverNames
+          );
+        });
+      });
     });
   });
 });
